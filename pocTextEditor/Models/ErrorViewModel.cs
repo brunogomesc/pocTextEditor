5be9fe4b6238e8196bd1fa712cf76cf3b4dd3b9f
Bruno Costa <Bruno Costa@DESKTOP-FRVBQSQ>
2024-08-29 18:59:27 -0300

    Add project files.

---
+++ pocTextEditor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pocTextEditor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
