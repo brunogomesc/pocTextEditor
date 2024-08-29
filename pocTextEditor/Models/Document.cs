6ad9c06aebfdd446962e370a19d8664476b3a058
Bruno Antonio Gomes da Costa <brunogomesc@outlook.com.br>
2024-08-29 20:27:02 -0300

    Clean the solution and create database context

---
+++ pocTextEditor/Models/Document.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pocTextEditor.Models
+{
+    public class Document
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Write the title!")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Write the content!")]
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
